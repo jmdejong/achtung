91038770c330e7b111e59850958e3f8dda30656b
troido <troido@hotmail.com>
2017-03-25 16:37:51 +0100

    a bot's checkpoints are now defined in the options

--- controllers.js
+++ controllers.js
@@ -44,18 +44,7 @@ class InputController {
 class WhiskerAIController {
     
     constructor(options){
-        this.points = [
-            {x: 80, y: 90, weight: .1},
-            {x: 30, y: 110, weight: .2},
-            {x: 10, y: 120, weight: .3},
-            {x: 35, y: 45, weight: .5},
-            {x: 20, y: 55, weight: 1},
-            {x: 7, y: 58, weight: 2},
-//             {x: 4, y: 60, weight: 1},
-            {x: 10, y: 35, weight: 4},
-            {x: 20, y: 30, weight: 6},
-            {x: 5, y: 10, weight: 15}
-        ];
+        this.points = options.controlpoints;
         for (let point of [...this.points]){
             this.points.push({x: -point.x, y: point.y, weight: -point.weight});
         }
